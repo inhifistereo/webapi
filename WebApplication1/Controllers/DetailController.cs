4d665d896103734a5a8a613899b51803598a5532
David Peterson <dapeters@microsoft.com>
2017-01-10 11:23:50 -0600

    Initial Commit

---
+++ WebApplication1/Controllers/DetailController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/xml")]
+    public class DetailController : Controller
+    {
+        Detail[] details = new Detail[]
+        {
+                new Detail{id = 1,name="John Johansen", Address="54 Oak", salary=50000},
+                new Detail{id = 2,name="Steve Smith",Address="84 Elm",salary= 40000},
+                new Detail {id = 3,name="Tyler Thompson",Address="26 Maple",salary=480000}
+        };
+
+        // GET: api/detail
+        [HttpGet]
+        public IEnumerable<Detail> Get()
+        {
+            return details;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using System.Xml;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.Swagger.Model;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();        
+
+            
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        private string GetSwaggerXMLPath()
+        {
+            var app = PlatformServices.Default.Application;
+            return System.IO.Path.Combine(app.ApplicationBasePath, "WebApplication1.xml");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc().AddXmlDataContractSerializerFormatters();
+
+            // Swagger services.
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Test API",
+                    Description = "David's Test API",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "TDS Test", Email = "dapeters@microsoft.com", Url = "http://www.microsoft.com" }
+                });
+                options.IncludeXmlComments(GetSwaggerXMLPath());
+                options.DescribeAllEnumsAsStrings();
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUi();
+        }
+    }
+}
